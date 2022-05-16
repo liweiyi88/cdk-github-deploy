49b3cb166fbc9057fec9d4e7bb1028ff5ad93218
liweiyi88 <weiyi.li713@gmail.com>
2022-05-16 15:03:50 +1000

    create string parameter

--- lib/cdk-github-deploy-stack.ts
+++ lib/cdk-github-deploy-stack.ts
@@ -1,28 +1,22 @@
 import { Stack, StackProps, Stage, StageProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm'
 
 export class CdkGithubDeployStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkGithubDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new StringParameter(this, 'GithubParam', {
+      parameterName: '/gh-deploy',
+      stringValue: 'julian-test'
+    })
   }
 }
 
-export interface AppStageProps extends StageProps {
-
-}
-
 export class AppStage extends Stage {
-  constructor(scope: Construct, id: string, props?: AppStageProps) {
+  constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props)
 
-    const appStack = new CdkGithubDeployStack(this, 'GithubDeployStack')
+    new CdkGithubDeployStack(this, 'GithubDeployStack')
   }
 }
